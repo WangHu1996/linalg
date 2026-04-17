4c75b73ca3bc478a20ea4b17baf1ca344ac3372b
王虎 <www.wh1131457765wh@foxmail.com>
2026-04-17 16:59:10 +0800

    使用FetchContent导入mdvector

--- src/demo.cc
+++ src/demo.cc
@@ -1,20 +1,11 @@
-#include "dense/dense.h"
+#include "include_md_all.h"
+
+#include <print>
 
 int main() {
-  print_simd_type();
-
-  // dense dynamic
-  std::println("dense dynamic 2x2:");
-  linalg::dense_dynamic<double> dense;
-  dense.set_shape({2, 2});
-  dense.fill(2);
-  dense.print();
-
-  // dense static
-  std::println("dense static 2x2:");
-  linalg::dense_static<double, 2, 2> dense_s;
-  dense_s.fill(3);
-  dense_s.print();
+  md::vector<double, 2> vec(3, 3);
+  vec.set_arange();
+  vec.print();
 
   return 0;
 }
